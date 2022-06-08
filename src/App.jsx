5436ddc1b3e7d72178c1dce574fe75da057e4a74
Rianfiore <rianfiore@gmail.com>
2022-06-08 02:35:50 -0300

    creating jsx files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { NavBar } from "./components/nav-bar";
+import { Main } from "./pages/main";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/main/index.jsx
@@ -0,0 +1,14 @@
+import * as S from "./styles";
+
+export const Main = () => {
+  return (
+    <S.Container>
+      <main>
+        <section>
+          <div className="background"></div>
+        </section>
+        <section></section>
+      </main>
+    </S.Container>
+  );
+};
