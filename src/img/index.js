854e4ed4fcad5a8ba53fe5165be9a33e95f51b7e
Rianfiore <rianfiore@gmail.com>
2022-06-09 02:55:04 -0300

    mocking all img data, adding new images

---
+++ src/img/index.js
@@ -0,0 +1,28 @@
+import libraIcon from "./libra.svg";
+import prismIcon from "./prism.svg";
+import bg1 from "./bg1.jpg";
+import mask from "./mask.png";
+import { MdFamilyRestroom, MdGroups, MdWork } from "react-icons/md";
+import { FaHandshake, FaChess, FaChalkboardTeacher } from "react-icons/fa";
+import { IoIosBusiness } from "react-icons/io";
+import { RiFileSearchFill, RiBankFill } from "react-icons/ri";
+import { HiCurrencyDollar } from "react-icons/hi";
+import { MdRealEstateAgent } from "react-icons/md";
+
+export {
+  libraIcon,
+  prismIcon,
+  bg1,
+  mask,
+  MdFamilyRestroom,
+  MdGroups,
+  MdWork,
+  FaHandshake,
+  FaChess,
+  FaChalkboardTeacher,
+  IoIosBusiness,
+  RiFileSearchFill,
+  RiBankFill,
+  HiCurrencyDollar,
+  MdRealEstateAgent,
+};
