8ed1a59723c219beb740d222f20fd3bbe1afb4c2
Rianfiore <rianfiore@gmail.com>
2022-06-10 02:26:17 -0300

    adding footer on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { NavBar } from "./components/navBar";
 import { Main } from "./pages/main";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Main />
+      <Footer />
     </div>
   );
 }
