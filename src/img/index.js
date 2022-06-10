ae260322d77cbd37e5bb8ea32a93b2bd85004322
Rianfiore <rianfiore@gmail.com>
2022-06-10 02:28:30 -0300

    removing mask, updating new icons

--- src/img/index.js
+++ src/img/index.js
@@ -1,19 +1,22 @@
 import libraIcon from "./libra.svg";
 import prismIcon from "./prism.svg";
 import bg1 from "./bg1.jpg";
-import mask from "./mask.png";
-import { MdFamilyRestroom, MdGroups, MdWork } from "react-icons/md";
+import {
+  MdFamilyRestroom,
+  MdGroups,
+  MdWork,
+  MdRealEstateAgent,
+} from "react-icons/md";
 import { FaHandshake, FaChess, FaChalkboardTeacher } from "react-icons/fa";
 import { IoIosBusiness } from "react-icons/io";
-import { RiFileSearchFill, RiBankFill } from "react-icons/ri";
+import { RiFileSearchFill, RiBankFill, RiShieldUserFill } from "react-icons/ri";
 import { HiCurrencyDollar } from "react-icons/hi";
-import { MdRealEstateAgent } from "react-icons/md";
+import { GiPistolGun } from "react-icons/gi";
 
 export {
   libraIcon,
   prismIcon,
   bg1,
-  mask,
   MdFamilyRestroom,
   MdGroups,
   MdWork,
@@ -23,6 +26,8 @@ export {
   IoIosBusiness,
   RiFileSearchFill,
   RiBankFill,
+  RiShieldUserFill,
   HiCurrencyDollar,
   MdRealEstateAgent,
+  GiPistolGun,
 };
