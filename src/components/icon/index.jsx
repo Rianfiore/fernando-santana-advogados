a6b41174a8a82f729f4a48900142b0d93d1a886a
Rianfiore <rianfiore@gmail.com>
2022-06-10 02:28:01 -0300

    creating icon component

---
+++ src/components/icon/index.jsx
@@ -0,0 +1,10 @@
+import { AiFillTrophy } from "react-icons/ai";
+import * as S from "./styles";
+
+export const Icon = () => {
+  return (
+    <S.Container>
+      <AiFillTrophy color="#0f1115" size="6vw" />
+    </S.Container>
+  );
+};
