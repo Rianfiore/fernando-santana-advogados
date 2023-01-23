c69eb05770987fab7923d7ca602061db4712c208
Rianfiore <rianfiore@gmail.com>
2023-01-23 17:13:41 -0300

    fix: add next and out folder to git ignore

--- components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Carousel } from "./carousel";
-export { default as Button } from "./button";
-export { default as Card } from "./card";
-export { default as Icon } from "./icon";
-export { default as Layout } from "./layout";
